cb4c9a35a10ff11bb044e648b7f30487a6c96177
Alfiya <al.yausheva@gmail.com>
2025-01-18 17:08:40 +0300

    selection sort and recurtion

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,6 +1,6 @@
 //Binary search
 
-let array = [1, 3, 5, 7, 9, 10, 11, 13];
+/*let array = [1, 3, 5, 7, 9, 10, 11, 13];
 let search = function(arr, target){
     let left = 0; 
     let right = arr.length - 1;
@@ -20,4 +20,38 @@ let search = function(arr, target){
     }
     return - 1 ;
 }
-console.log(search(array, 10))
+console.log(search(array, 10))*/
+
+//Selection sort
+
+
+
+/*function selectionSort(arr) {
+    const {length} = arr;
+    for (let i = 0; i < (length - 1); i++){
+        let minIndex = i;
+        for (let j = i + 1; j < length; j++) {
+            if (arr[minIndex] > arr[j]){
+                minIndex = j;
+            }
+        }
+    if (minIndex!==i){
+            const temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex]= temp;
+        } 
+    }
+    return arr;
+}
+const arr = [3, 2, 6, 4, 8, 5, 9, 10];
+const result = selectionSort(arr);
+console.log(result);*/
+
+// Recurtion
+
+let t=0;
+function f1(){
+    t++;
+    if (t === 100) return
+    f1();
+}
