741664dd05f32c290a4c082accbf8568740c46c3
Alfiya <al.yausheva@gmail.com>
2025-01-28 16:19:34 +0300

    wordsCount

--- tasks.js
+++ tasks.js
@@ -6,3 +6,20 @@ let perimetr = (a+a)*2;
 document.getElementById('result1').innerHTML=square;
 document.getElementById('result2').innerHTML=perimetr;
 }
+function wordsCount(){
+    let text = document.getElementById('mytext').value;
+    let numWords = 0;
+    for (let i = 0; i < text.length; i++){
+        let currentCharacter = text[i];
+
+        if (currentCharacter == " ") {
+            numWords += 1;
+        }
+    }
+    numWords += 1;
+    document.getElementById('wordscount').innerHTML= "Количество слов: " + numWords;  
+
+
+}
+
+
