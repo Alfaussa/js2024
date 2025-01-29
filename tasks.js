8dbf2296dc83e36987ddd5cd0bad5af4a206fc75
Alfiya <al.yausheva@gmail.com>
2025-01-29 17:16:13 +0300

    str reverse

--- tasks.js
+++ tasks.js
@@ -1,4 +1,4 @@
-
+/*
 function func (){
 let a = Number(document.getElementById('num').value);
 let square = a*a;
@@ -21,5 +21,23 @@ function wordsCount(){
 
 
 }
+let a = 'milk';
+let b = 'mold';
+function func(){
+    if (a[0] == b[0]){
+        console.log('mTCH');
+    }else{
+        console.log("no match")
+    }
+}
+func();*/
 
+let str = 'строка';
 
+function func(){
+    for(let i = str.length - 1; i >= 0; i --){
+  console.log(str[i]) ;
+       
+}
+}
+func()
